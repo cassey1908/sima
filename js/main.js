d778bc93dbb9a778b1a48b2fd7e8f113f89f585e
cassey1908 <casandracasseyayi@gmail.com>
2024-06-30 23:29:23 +0100

    confirmado

---
+++ js/main.js
@@ -0,0 +1,64 @@
+//var casa 
+//casa="hola"
+//alert(casa);
+//console.log(casa);
+
+/*var numero1
+var numero2
+var resultado
+numero1=4
+numero2=6
+resultado=numero1-numero2
+alert("el resultado es :"  +resultado);
+//console.log(resultado);
+//alert(resultado);
+//document.write(resultado);*/
+
+
+//Instancia: metodo tradicional
+
+/*function casa(){
+    console.log("metodo tradicional")
+}*/
+
+ /*function coche (nombre, apellido, edad, peso){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.edad=edad;
+    this.peso=peso;
+
+    console.log(" tu nombre es : " + this.nombre + " , tu apellido es : " + this.apellido + " , tu edad es : " + this.edad + "  y tu peso es de : " + this.peso);
+ }
+
+ var color=new coche ("carla" , "nguema " , 23, 54)*/
+
+
+
+ //**** metodo moderno ****
+
+ /*class colegio{
+    constructor(){
+        this.nombre= "eco";
+        console.log(this.nombre);
+    }
+ }
+
+ var nombre1= new colegio();*/
+
+
+ //****Objetos */
+
+
+
+ let hola=document.getElementById("rec");
+
+ console.log(hola);
+hola.addEventListener("click", function recarga(){
+    window.prompt("me llamo carla");
+    //alert("hola me llamo carla");
+})
+
+
+
+
+ 
